f2d7c473ef0909324eb88836a661487f1d87569f
Dirceu Junior <dirceujunior@MacBook-Pro-de-Dirceu.local>
2017-06-22 10:19:20 -0300

    Staging mas o Lomadee está dando erro no upload.

--- Responsive_Assets/build.py
+++ Responsive_Assets/build.py
@@ -6,7 +6,7 @@ import re
 REMOVE_WS = re.compile(r"\s{2,}").sub
 output_file_name = "vitrine_responsiva-1.5.5.min.js"
 
-os.system("java -jar ~/Novos\ Arquivos/Projetos/Coora/tools/yuicompressor-2.4.8.jar ./style.css -o style-min.css -v")
+os.system("java -jar ~/Novos\ Arquivos/Projetos/Coora/tools/yuicompressor-2.4.8.jar ./style.css -o ./staging/style-min.css -v")
 
 
 os.system("java -jar ~/Novos\ Arquivos/Projetos/Coora/tools/compiler-latest/closure-compiler-v20170521.jar --js ./lib/json2.js ./lib/spin.js ./vitrine_responsiva.js --js_output_file " + output_file_name)
