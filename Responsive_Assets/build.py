e5420124b327541775e355c1916371822ccb2795
dirceupj <dirceuu@gmail.com>
2019-03-08 22:14:00 -0300

    update closure-compiler version to latest

--- Responsive_Assets/build.py
+++ Responsive_Assets/build.py
@@ -9,7 +9,7 @@ output_file_name = "vitrine_responsiva-1.7.2.min.js"
 os.system("java -jar ../tools/yuicompressor-2.4.8.jar ./style.css -o ./staging/style-min.css -v")
 
 
-os.system("java -jar ../tools/closure-compiler-v20190121.jar --js ./lib/json2.js ./lib/spin.min.js ./vitrine_responsiva.js --js_output_file " + output_file_name)
+os.system("java -jar ../tools/closure-compiler-v20190301.jar --js ./lib/json2.js ./lib/spin.min.js ./vitrine_responsiva.js --js_output_file " + output_file_name)
 
 
 os.system("mv ./" + output_file_name + " ./staging/")
