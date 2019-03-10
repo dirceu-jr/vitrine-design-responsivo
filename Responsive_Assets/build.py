aa857fe33802ccf7d0beab83a18da74df90ac5fc
dirceupj <dirceuu@gmail.com>
2019-03-09 21:26:58 -0300

    automatically update inline css from ./staging/frameads.html

--- Responsive_Assets/build.py
+++ Responsive_Assets/build.py
@@ -1,13 +1,35 @@
-# minimize CSS; minimize JS
-
 import os
 import re
 
-output_file_name = "vitrine_responsiva-1.7.2.min.js"
+output_file_name = "./staging/vitrine_responsiva-1.7.2.min.js"
 
+# minimize CSS using YUI Compressor - stores at ./staging/
 os.system("java -jar ../tools/yuicompressor-2.4.8.jar ./style.css -o ./staging/style-min.css -v")
 
+# minimize JS using Closure Compiler - stores at ./staging/
 os.system("java -jar ../tools/closure-compiler-v20190301.jar --js ./lib/json2.js ./lib/spin.min.js ./vitrine_responsiva.js --js_output_file " + output_file_name)
 
+# open css file
+css_file = open("./staging/style-min.css", "r")
+css_content = css_file.read()
+css_file.close()
 
-os.system("mv ./" + output_file_name + " ./staging/")
+# open frameads.html
+frameads_file = open("./staging/frameads.html", "r+")
+frameads_content = frameads_file.read()
+
+link_el = r"\<style\>(.*)<\/style\>"
+style = "<style>" + css_content + "</style>"
+
+regex = re.compile(link_el)
+
+# replace
+frameads_content = re.sub(regex, style, frameads_content)
+
+# erase content
+frameads_file.seek(0)
+frameads_file.truncate()
+
+# write replaced
+frameads_file.write(frameads_content)
+frameads_file.close()
